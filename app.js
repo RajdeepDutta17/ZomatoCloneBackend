8c6079c67411fafeefbdb1f6d443f1f496bc24bd
RajdeepDutta17 <dutta10rajdeep@gmail.com>
2023-09-25 20:16:16 +0530

    allow origin changes

--- app.js
+++ app.js
@@ -5,25 +5,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 10000;
 const uri = process.env.DATABASE_URL;
 
-const whitelist = [
-  "http://localhost:3000",
-  "https://zomatoclonefrontend.onrender.com",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/", route);
