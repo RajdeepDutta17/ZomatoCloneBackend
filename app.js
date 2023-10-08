23b860cb538a0a76653348132f16e2635b8c29ab
RajdeepDutta17 <dutta10rajdeep@gmail.com>
2023-10-08 10:38:18 +0530

    changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/", route);
