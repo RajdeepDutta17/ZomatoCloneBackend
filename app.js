096d9c80465a7634aaea7c749aca88e8af0a2b3e
RajdeepDutta17 <dutta10rajdeep@gmail.com>
2023-10-10 13:17:52 +0530

    changes made

--- app.js
+++ app.js
@@ -25,15 +25,15 @@ const uri = process.env.DATABASE_URL;
 
 app.use(express.json());
 app.use(express.static("public"));
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://zomatoclonefrontend.onrender.com"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://zomatoclonefrontend.onrender.com"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 app.use("/", route);
 
 mongoose
